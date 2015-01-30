fabf37d3193445b4f7746662ed6a363faa5bd33f
cobachie <tanacobachie@gmail.com>
2015-01-30 10:07:28 +0900

    Reword user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
 
   def display_auth
   	return unless self.auth
-  	%w(システム管理者 職員 職員+会員 会員)[self.auth-1]
+  	%w(システム管理者 職員 職員+生産者 生産者)[self.auth-1]
   end
 
   def self.auth_seloptions
-  	[['システム管理者', 1], ['職員', 2], ['職員+会員', 3], ['会員', 4]]
+  	[['システム管理者', 1], ['職員', 2], ['職員+生産者', 3], ['生産者', 4]]
   end
 
   def self.seloptions
